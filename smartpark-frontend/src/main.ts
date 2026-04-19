cd455c6782b7f90f10cc6c7b1ba78dab130d49d3
tomposgergo2 <tompos.gergo@students.jedlik.eu>
2026-04-19 19:01:31 +0200

    kész

--- smartpark-frontend/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter, withHashLocation } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
-
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes, withHashLocation()), provideAnimations()]
-}).catch(err => console.error(err));
